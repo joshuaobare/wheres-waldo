de93dbd15a3866e2233d60394aa824436a567426
joshuaobare <jobare.obare@gmail.com>
2022-10-24 19:25:26 +0300

    added waldo image and barebones nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import image from "./images/whereswaldo.jpg"
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <img src={image} alt="" />
       
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+export default function Nav() {
+    return (
+        <nav>
+            <h1>Where's Waldo?</h1>
+            <div className="nav-list">
+                <ul>
+                    <li>Home</li>
+                    <li>Leaderboards</li>                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
